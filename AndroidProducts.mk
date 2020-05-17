98832a70e77e864eabaee61e301d0f705277c08e
yincen17 <yincen17@users.noreply.github.com>
2020-05-17 06:59:04 +0000

    tissot: Candy bringup
    Signed-off-by: yincen17 <yincen17@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tissot.mk
+    $(LOCAL_DIR)/candy_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tissot-user \
-    derp_tissot-userdebug \
-    derp_tissot-eng \
+    candy_tissot-user \
+    candy_tissot-userdebug \
+    candy_tissot-eng \
